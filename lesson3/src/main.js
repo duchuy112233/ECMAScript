b77d0b3c3b46ca0d337b157d341f1af15b4739e8
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-01-16 11:04:21 +0700

    up

--- lesson3/src/main.js
+++ lesson3/src/main.js
@@ -1,28 +1,21 @@
-import sukien from "./pages/sukien";
+// Library - Thư viện
+import { render, router } from "./ultilities";
+
+// Components
 import Contact from "./pages/contact";
 import HomePage from "./pages/homepage"
-import Navigo from 'navigo'; // When using ES modules.
 import NotFound from "./pages/notFound";
 
-const router = new Navigo('/');
-// // BOM
-// console.log(window.location);
-
 router.on('/', function () {
-  document.querySelector("#app").innerHTML = HomePage()
+  render("#app", HomePage)
 });
 
 router.on('/contact', function () {
-  document.querySelector("#app").innerHTML = Contact()
-});
-router.on('/sukien', function () {
-  document.querySelector("#app").innerHTML = sukien()
+  render("#app", Contact)
 });
 
 router.notFound(function () {
-  document.querySelector("#app").innerHTML = NotFound()
+  render("#app", NotFound)
 })
-
-
 router.resolve();
 // 
