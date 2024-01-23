ec6268146632b0a6d7a5567115a45a91e1aa0f3f
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-01-23 11:10:24 +0700

    Update main.js

--- asmecma/src/main.js
+++ asmecma/src/main.js
@@ -3,15 +3,11 @@ import { books } from "../db.json" assert {type:'json'};
 import HomePage from "./pages/homePages";
 import DetailProductPage from "./pages/detailProduct";
 import Products from "./components/products";
-router.on({
-  "/": () => {
-    render("#app", HomePage(books));
-  },
-  "/book/detail/:id": (data) => {
-    render("main", DetailProductPage(books, data.data.id));
-    window.scrollTo(0, 0);
-  }
-});
+
+const routeHandlers = new Map([
+  ["/", () => render("#app", HomePage(books))],
+  ["/book/detail/:id", ({ data }) => render("main", DetailProductPage(books, data.id))],
+]);
+for (const [route, handler] of routeHandlers) router.on({ [route]: handler });
 
 router.resolve();
-
